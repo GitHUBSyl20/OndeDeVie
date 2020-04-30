0b0764431a143d8bdc514377c8d8a6a02f6b70cb
sylvain <sylvainmagana@ymail.com>
2020-04-30 16:55:40 +0200

    centerd text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,6 @@ import { MatSliderModule } from '@angular/material/slider';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { FooterComponent } from './footer/footer.component';
@@ -26,7 +25,6 @@ import { HeaderComponent } from './header/header.component';
 import { SoinsChamaniquesComponent } from './soins-chamaniques/soins-chamaniques.component';
 import { ContactInfoComponent } from './contact-info/contact-info.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { TemoignageComponent } from './temoignage/temoignage.component';
 import { TemoignagesComponent } from './temoignages/temoignages.component';
 
 
@@ -47,7 +45,6 @@ import { TemoignagesComponent } from './temoignages/temoignages.component';
     SoinsChamaniquesComponent,
     ContactInfoComponent,
     PageNotFoundComponent,
-    TemoignageComponent,
     TemoignagesComponent
   ],
   imports: [
